bc8164472ef63c1a46901e21a6757883f4dbffe2
ashfaq867 <97423424+ashfaq867@users.noreply.github.com>
2022-12-14 13:00:50 +0500

    Update login.php

--- User Form Restful APIs Assignment/login.php
+++ User Form Restful APIs Assignment/login.php
@@ -14,37 +14,35 @@ $encrytPassword=sha1($password);
 $email=$data['email'];
 $token=random_bytes(15);
 $valid_token=bin2hex($token);
-$sql="SELECT id, email, password FROM users WHERE email='$email'";
+
+//query to fetch data using login credentials
+$sql="SELECT email, password FROM users WHERE email='$email' AND password='{$encrytPassword}'";
 $result=mysqli_query($conn,$sql);
 if(mysqli_num_rows($result)>0){
     $user=mysqli_fetch_assoc($result);
     session_start();
-     
+     //check if there is no token or login available then create session and token for specific time
     if(!isset($_SESSION['token']) || time()>$_SESSION['expire']){
-        if($user['password']==$encrytPassword && $user['email']==$email){        
+               
         $_SESSION['token']=$valid_token;
         $_SESSION['email']=$email;
-        $_SESSION['id']=$user['id'];
          // Taking current system Time
          $_SESSION['start'] = time(); 
   
-         // Destroying session after 1 minute
+         // Destroying session after 2 minute    expiry time can be change according to requirment
          $_SESSION['expire'] = $_SESSION['start'] + 120 ;
         echo json_encode(array("status"=>true, "Message:"=>"You are login",
         "Token:"=>"Token is Valid for 2 minute","Token_Value"=>$valid_token));
     
         }
-        else{
-            echo json_encode(array("status"=>false, "Message:"=>"Your email/password is incorrect"));
-        }
-
-    }
+    //if user already login then show the message below
     else{
         echo json_encode(array("status"=>false, "Message:"=>"You Are already Loggedin"));
     }
 }
+//if credentials not match then show the message below
 else{
-echo json_encode(array("status"=>false, "Message:"=>"No such email found!"));
+    echo json_encode(array("status"=>false, "Message:"=>"Your email/password is incorrect"));
 }
 
 ?>
